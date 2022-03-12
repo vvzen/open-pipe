8c3c1902954d3c7f2e847e53d4bd8109bc2d745c
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-12 12:45:10 +0000

    Add naming module

---
+++ openpipe/naming.py
@@ -0,0 +1,72 @@
+"""
+This module is concerned with parsing strings and understanding if they match
+some internal (customizable) naming convention.
+"""
+
+import re
+
+from openpipe.config import get_config, MalformedConfigError
+
+def get_naming_regexes(name):
+
+    regexes_patterns = []
+
+    naming_config = get_config("show_naming")
+    naming_entry = naming_config.get(name)
+    if not naming_entry:
+        return
+
+    patterns = naming_entry.get("patterns")
+    if not patterns:
+        raise MalformedConfigError("Naming entry for '%s' "
+                                  "didn't contain a 'patterns' key." % name)
+
+    compound = naming_entry.get("appends_to")
+    if compound:
+        appends_with = naming_entry.get("appends_with")
+        if not appends_with:
+            raise MalformedConfigError(
+                    "Naming entry for '%s' is compounded, "
+                    "but is not defining the required 'appends_with' key.")
+
+        other_entry = naming_config.get(compound)
+        if not other_entry:
+            raise MalformedConfigError("Naming entry for '%s' is compounded, "
+                                      "but the other entry specified ('%s') "
+                                      "doesn't exist." % (name, compound))
+
+        other_entry_patterns = other_entry.get("patterns")
+        if not other_entry_patterns:
+            raise MalformedConfigError(
+                             "Naming entry for '%s' is compounded, but "
+                             "the 'pattern' of '%s' isn't defined" %
+                             (name, other_entry))
+
+        for pattern in patterns:
+            for other_pattern in other_entry_patterns:
+                regexes_patterns.append(
+                    "^" + other_pattern + appends_with + pattern + "$")
+    else:
+        for pattern in patterns:
+            regexes_patterns.append("^" + pattern + "$")
+
+    if not regexes_patterns:
+        return
+
+    return [re.compile(p) for p in regexes_patterns]
+
+
+def is_shot(input_name):
+    shot_regexes = get_naming_regexes("shot")
+    for shot_regex in shot_regexes:
+        if shot_regex.match(input_name):
+            return True
+    return False
+
+
+def is_sequence(input_name):
+    sequence_regexes = get_naming_regexes("sequence")
+    for sequence_regex in sequence_regexes:
+        if sequence_regex.match(input_name):
+            return True
+    return False
