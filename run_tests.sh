f7fec74375aa6a21f0628ec421a7c3d4470ab98c
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-12 13:09:43 +0000

    Temp commit

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,8 @@
 echo "Appending openpipe to PYTHONPATH"
 source ./openpipe/source_me.bash
 echo "Running Open Pipe Unit Test suite"
-pytest openpipe/test --cov=openpipe --cov-report=xml
+if [ ! -z $DEBUG ]; then
+    pytest openpipe/test --cov=openpipe
+else
+    pytest openpipe/test --cov=openpipe --cov-report=xml
+fi
