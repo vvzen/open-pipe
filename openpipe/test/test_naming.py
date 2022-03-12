1b7cee4ac9ec60535a458eb1b39c1e4202a1f09e
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-12 13:34:33 +0000

    Add more unit tests for the naming and config

--- openpipe/test/test_naming.py
+++ openpipe/test/test_naming.py
@@ -1,6 +1,14 @@
+import os
+
 import pytest
 
-from openpipe.naming import is_shot, is_sequence
+import openpipe.config
+from openpipe.naming import is_shot, is_sequence, get_naming_regexes
+
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+SAMPLES_DIR = os.path.join(CURRENT_DIR, "sample_files")
+
 
 @pytest.mark.parametrize("name,result", [
     pytest.param("sc010_0010", True),
@@ -24,3 +32,23 @@ def test_is_shot_with_default_config(name, result):
 ])
 def test_is_sequence_with_default_config(name, result):
     assert is_sequence(name) == result
+
+
+@pytest.mark.parametrize("config_path", [
+    pytest.param(os.path.join(SAMPLES_DIR, "configs", "show_naming_compound_undefined_patterns.yml")),
+    pytest.param(os.path.join(SAMPLES_DIR, "configs", "show_naming_missing_shot_pattern.yml")),
+    pytest.param(os.path.join(SAMPLES_DIR, "configs", "show_naming_missing_appends_with.yml")),
+])
+def test_naming_of_malformed_configs(monkeypatch, config_path):
+
+    def mocked_config_path(name):
+        return config_path
+
+    monkeypatch.setattr(openpipe.config, "get_config_path", mocked_config_path)
+
+    with pytest.raises(openpipe.config.MalformedConfigError):
+        is_shot("name is irrelevant here")
+
+
+def test_get_naming_regexes_missing_entry():
+    assert get_naming_regexes("a_name_that_is_not_a_valid_key") is None
