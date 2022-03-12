c1abae45a46b6360c123f7d5cf3afc3953b6a54b
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-12 13:16:46 +0000

    Update source scripts to set the config env too

--- openpipe/source_me.bash
+++ openpipe/source_me.bash
@@ -1,10 +1,19 @@
 # Source this file to append the library to your pythonpath
-new_python_path=$(realpath .)
-result=$(echo $PYTHONPATH | grep $new_python_path -c)
 
-if [ $result == "1" ]; then
-    echo "Path is already set correctly, skipping"
-    exit 1
+new_python_path=$(realpath ..)
+python_path_already_appended=$(echo $PYTHONPATH | grep $new_python_path -c)
+
+if [ $python_path_already_appended == "1" ]; then
+    echo "PYTHONPATH is already set correctly, skipping"
+else
+    export PYTHONPATH="$new_python_path:$PYTHONPATH"
 fi
 
-export PYTHONPATH="$new_python_path:$PYTHONPATH"
+new_config_path=$(realpath ./openpipe/configs)
+config_path_already_appended=$(echo $OPENPIPE_CONFIG_PATH| grep $config_path_already_appended -c)
+
+if [ $config_path_already_appended == "1" ]; then
+    echo "OPENPIPE_CONFIG_PATH is already set correctly, skipping"
+else
+    export OPENPIPE_CONFIG_PATH="$new_config_path:$OPENPIPE_CONFIG_PATH"
+fi
--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,8 @@
 
 echo "Appending openpipe to PYTHONPATH"
 source ./openpipe/source_me.bash
+echo "$PYTHONPATH"
+echo "$OPENPIPE_CONFIG_PATH"
 echo "Running Open Pipe Unit Test suite"
 if [ ! -z $DEBUG ]; then
     pytest openpipe/test --cov=openpipe
