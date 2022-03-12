6e9caef487003b911f81abee8959ab1afaeeb886
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-12 12:43:10 +0000

    Add initial draft of 'op' CLI

---
+++ bin/README.md
@@ -0,0 +1,27 @@
+# README
+
+## op: a no frills filesystem navigation script written in fish
+
+### How to use
+
+From a terminal where `$SHELL` is `fish`, simply create an alias called 'op', that sources the `op.fish` file, like this:
+```
+alias op="source /whereever/you/installed/open-pipe/openpipe/bin/op.fish"
+```
+
+### Why fish?
+
+'Cause fish is exactly what it says it is: a Friendly Interactive SHell.
+Fish makes the scripting a pleasure, and it's incredibly user-friendly thanks to its built-in autocompletions.
+Customizing fish is as easy as typing `fish_config` in your terminal.
+The hope is to give a easy to use and inclusive shell that the consumers of OpenPipe will love.
+
+While any more 'serious' script that has to be portable/POSIX-compliant will be written in bash, the filesystem navigation is a simple enough task that can be achieved in a more adventerous and user-friendly way thanks to fish.
+
+Fish also has good support for variable scoping.
+
+The intention of OpenPipe currently is to use bash for heavy lifting back-end stuff, and fish for anything that it's user-facing.
+
+Further reading:
+- https://betterprogramming.pub/fish-vs-zsh-vs-bash-reasons-why-you-need-to-switch-to-fish-4e63a66687eb
+- https://arstechnica.com/information-technology/2005/12/linux-20051218/3/
---
+++ bin/op.fish
@@ -0,0 +1,63 @@
+# IMPORTANT: This script needs to be sourced, not executed
+
+# NB: any CLI flag that we add via these options will be available as '_flag_something'
+# For example: 'help' -> '_flag_help', etc.
+set -l options (fish_opt --short h --long help)
+
+# Additional args can be added like this:
+#set options $options (fish_opt --short=m --long=max --required-val)
+
+# Parse the cli args
+argparse $options -- $argv
+
+function _help
+    echo -e "\nUSAGE"
+    echo "op is a portable" (basename $SHELL) "script used to navigate the filesystem."
+    echo "op takes care of going to a target location and setting the correct environment."
+    echo -e "\nEXAMPLES:"
+    echo -e "\$ op go /some/abs/path"
+    echo -e "\$ op go ../some/relative/path"
+    echo -e "\n"
+    exit 1
+end
+
+if set -q _flag_help
+   _help
+end
+
+# Implementation of filesystem navigation functionality
+function _go
+    set destination_path $argv
+
+    # Check destination is valid
+    if not test -d $destination_path
+        echo -e "Cannot 'go': $destination_path does not exist."
+        return
+    end
+
+    # Parse destination directory to understand environment
+
+    # Set environment variables
+    set --global --export --prepend --path OPENPIPE_CONFIG_PATH $destination_path
+
+    # Finally.. go
+    echo "Going to" $destination_path
+    cd $destination_path
+end
+
+# No subcommands provided?
+if test (count $argv) -eq 0
+    echo -e "\nop: no arguments provided!\n"
+    _help
+    exit 1
+end
+
+# Parse subcommands
+switch $argv[1]
+    case go
+        if test (count $argv) -eq 1
+            echo "'go' requires a destination directory (where you do you wanna go?)"
+            exit 1
+        end
+        _go $argv[2]
+end
