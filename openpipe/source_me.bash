d6197941df9e3f8b27e56c96acbc9efd03cb9344
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-18 17:00:28 +0000

    Improve script forcing [[]] evaluation

--- openpipe/source_me.bash
+++ openpipe/source_me.bash
@@ -6,8 +6,8 @@ new_python_path="$this_dir"
 if [ -z "$PYTHONPATH" ]; then
     export PYTHONPATH="$new_python_path"
 else
-    python_path_already_appended=$(echo $PYTHONPATH | grep $new_python_path -c)
-    if [ $python_path_already_appended == "1" ]; then
+    python_path_already_appended=$(echo "$PYTHONPATH" | grep $new_python_path -c)
+    if [[ $python_path_already_appended == "1" ]]; then
         echo "PYTHONPATH is already set correctly, skipping"
     else
         export PYTHONPATH="$new_python_path:$PYTHONPATH"
@@ -20,7 +20,7 @@ if [ -z "$OPENPIPE_CONFIG_PATH" ]; then
     export OPENPIPE_CONFIG_PATH="$new_config_path"
 else
     config_path_already_appended=$(echo $OPENPIPE_CONFIG_PATH | grep $config_path_already_appended -c)
-    if [ $config_path_already_appended == "1" ]; then
+    if [[ $config_path_already_appended == "1" ]]; then
         echo "OPENPIPE_CONFIG_PATH is already set correctly, skipping"
     else
         export OPENPIPE_CONFIG_PATH="$new_config_path:$OPENPIPE_CONFIG_PATH"
