9222c967e2409982ba2b096f5f40619b508915c7
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-18 11:35:42 +0000

    Better support case where paths are undefined

--- openpipe/source_me.bash
+++ openpipe/source_me.bash
@@ -1,19 +1,27 @@
 # Source this file to append the library to your pythonpath
 
 new_python_path=$(realpath ..)
-python_path_already_appended=$(echo $PYTHONPATH | grep $new_python_path -c)
 
-if [ $python_path_already_appended == "1" ]; then
-    echo "PYTHONPATH is already set correctly, skipping"
+if [ -z $PYTHONPATH ]; then
+    export PYTHONPATH="$new_python_path"
 else
-    export PYTHONPATH="$new_python_path:$PYTHONPATH"
+    python_path_already_appended=$(echo $PYTHONPATH | grep $new_python_path -c)
+    if [ $python_path_already_appended == "1" ]; then
+        echo "PYTHONPATH is already set correctly, skipping"
+    else
+        export PYTHONPATH="$new_python_path:$PYTHONPATH"
+    fi
 fi
 
 new_config_path=$(realpath ./openpipe/configs)
-config_path_already_appended=$(echo $OPENPIPE_CONFIG_PATH| grep $config_path_already_appended -c)
 
-if [ $config_path_already_appended == "1" ]; then
-    echo "OPENPIPE_CONFIG_PATH is already set correctly, skipping"
+if [ -z $OPENPIPE_CONFIG_PATH ]; then
+    export OPENPIPE_CONFIG_PATH="$new_config_path"
 else
-    export OPENPIPE_CONFIG_PATH="$new_config_path:$OPENPIPE_CONFIG_PATH"
+    config_path_already_appended=$(echo $OPENPIPE_CONFIG_PATH | grep $config_path_already_appended -c)
+    if [ $config_path_already_appended == "1" ]; then
+        echo "OPENPIPE_CONFIG_PATH is already set correctly, skipping"
+    else
+        export OPENPIPE_CONFIG_PATH="$new_config_path:$OPENPIPE_CONFIG_PATH"
+    fi
 fi
