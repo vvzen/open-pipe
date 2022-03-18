df98cefee2899cba5cc687de331f5d553db77839
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-18 17:38:43 +0000

    op show -> op show-env
    - Rename op show
    - It will now print the information about the last env where you did a
    'op go' to, not the cwd
    - Keep track of the last go via a env variable called OPENPIPE_LAST_GO

--- openpipe/source_me.fish
+++ openpipe/source_me.fish
@@ -1,7 +1,9 @@
 # Source this file when you're doing dev stuff!
 
+set this_dir (realpath (dirname (status -f)))
+
 # Set the PATH
-set new_path (realpath ./bin)
+set new_path (realpath "$this_dir/../bin")
 set path_is_already_appended (echo $PATH | grep $new_path -c)
 
 if test $path_is_already_appended -eq 1
@@ -11,7 +13,7 @@ else
 end
 
 # Set the PYTHONPATH
-set new_python_path (realpath .)
+set new_python_path (realpath "$this_dir/..")
 set pythopath_is_already_appended (echo $PYTHONPATH | grep $new_python_path -c)
 
 if test $pythopath_is_already_appended -eq 1
@@ -21,7 +23,7 @@ else
 end
 
 # Set the config path
-set new_config_path (realpath (dirname (status -f))/configs)
+set new_config_path "$this_dir/configs"
 set config_already_appended (echo $OPENPIPE_CONFIG_PATH | grep $new_config_path -c)
 
 if test $config_already_appended -eq 1
