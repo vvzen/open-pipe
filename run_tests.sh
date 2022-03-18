83b0ee8625d0466c85f98b639b5c14059cb8079e
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-18 11:38:21 +0000

    Print out coverage when in DEBUG too

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ echo "$PYTHONPATH"
 echo "$OPENPIPE_CONFIG_PATH"
 echo "Running Open Pipe Unit Test suite"
 if [ ! -z $DEBUG ]; then
-    pytest openpipe/test --cov=openpipe
+    pytest openpipe/test --cov=openpipe --cov-report term-missing
 else
     pytest openpipe/test --cov=openpipe --cov-report=xml
 fi
