3e4f5f512ac9d70d82b138ec6c5d4b790abbaad8
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-03-25 16:49:42 +0000

    Add branch for detecting gitlab CI

--- run_tests.sh
+++ run_tests.sh
@@ -11,8 +11,14 @@ echo "PYTHONPATH: $PYTHONPATH"
 echo "OPENPIPE_CONFIG_PATH: $OPENPIPE_CONFIG_PATH"
 echo -e "\n"
 echo "Running Open Pipe Unit Test suite"
+
+# Are we running on Github?
 if [ -z $GITHUB_ACTION ]; then
     pytest tests --cov=openpipe --cov-report term-missing
+# Or on Gitlab?
+elif [ -z $CI ]; then
+    poetry run pytest tests --cov=openpipe --cov-report term-missing
+# Or locally?
 else
-    poetry run pytest tests --cov=openpipe --cov-report=xml
+    pytest tests --cov=openpipe --cov-report=xml
 fi
