d2d6912decd8279bb0bc1e45d69ebe7d09d104a8
Valerio Viperino <valerio.viperino@framestore.com>
2022-04-08 13:41:41 +0100

    Initial support for setting custom env vars on go

---
+++ openpipe/hooks/env_vars.py
@@ -0,0 +1,16 @@
+#!/bin/env python
+
+def main():
+    # Ftrack env vars
+    print(('FTRACK_API_KEY='
+           'ZWFjZDliMjktZTVjOC00N2I1LTk5NTYtZDg3YjBmMThkZDAxOjpjZWZ'
+           'mMDcyMi1jNDg0LTRiOTUtYjdiZS1hNjU1ODYyMjVmNWI'))
+    print(('SERVER_URL='
+           'https://m74-srl.ftrackapp.com'))
+    print(('FTRACK_API_USER='
+           'valerio.viperino@gmail.com'))
+
+    # OCIO
+
+if __name__ == "__main__":
+    main()
