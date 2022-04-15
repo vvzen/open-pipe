eafcbba4c1a6653cd03503bdb0f1d409bed43e65
Valerio Viperino <valerio.viperino@framestore.com>
2022-04-15 13:01:09 +0100

    Add tests for seq/shot. Improve fakefs handling
    - Always explicitely use the Patcher() context manager when there is the
      need to run a function within the fake filesystem

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
 import os
 
+
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 SAMPLES_DIR = os.path.join(CURRENT_DIR, "sample_files")
+
+
+def add_openconfig_configs_to_pyfakefs(fs):
+    current_config_path = os.getenv("OPENPIPE_CONFIG_PATH")
+    paths_to_search = current_config_path.split(":")
+
+    for path in paths_to_search[::-1]:
+        fs.add_real_directory(path)
