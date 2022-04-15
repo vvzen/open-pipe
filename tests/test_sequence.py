f909ec136724553884bf7323b191e58ed0eee10c
Valerio Viperino <valerio.viperino@framestore.com>
2022-04-15 15:59:51 +0100

    Add test for invalid shots/sequences

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -17,3 +17,15 @@ def test_create_sequence(monkeypatch, sequence_name):
     with Patcher() as patcher:
         add_openconfig_configs_to_pyfakefs(patcher.fs)
         create_sequence(sequence_name, raise_exc=True)
+
+@pytest.mark.parametrize("sequence_name", [
+    pytest.param("ASsc020"),
+    pytest.param("SOMEpreviz010"),
+])
+def test_create_invalid_sequence(monkeypatch, sequence_name):
+    monkeypatch.setenv("OPENPIPE_SHOW", "a_test_show")
+
+    with pytest.raises(ValueError):
+        with Patcher() as patcher:
+            add_openconfig_configs_to_pyfakefs(patcher.fs)
+            create_sequence(sequence_name, raise_exc=True)
