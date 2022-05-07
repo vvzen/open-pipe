b838a40e04e5ce4e27bb5c39ec3a9f8295c29576
Valerio Viperino <valerio.viperino@framestore.com>
2022-05-07 19:13:04 +0100

    Use '*' to indicate template partials (not '$')
    - This lets openpipe support the usage of environment variables via
    the classic '$' notation.
    - Other tweaks and improvements to documentation

--- openpipe/templates.py
+++ openpipe/templates.py
@@ -1,3 +1,4 @@
+import os
 import openpipe.show
 from openpipe.config import get_config
 
@@ -11,9 +12,6 @@ def get_template(name):
 
 
 def analyze_template(template_str):
-
-    "{shows_root}/{show_name}/{context}/work"
-    "{$default_workfile}.mb"
 
     tokens = []
     partials = []
@@ -50,7 +48,7 @@ def analyze_template(template_str):
             continue
 
         if token_has_started:
-            if char == "$":
+            if char == "*":
                 partial_has_started = True
                 continue
             else:
@@ -65,7 +63,7 @@ def flatten_template(template):
 
     for partial_name in partials:
         template_partial = get_template(partial_name)
-        partial_token = "{$%s}" % (partial_name)
+        partial_token = "{*%s}" % (partial_name)
         template = template.replace(partial_token, template_partial)
 
     return template
@@ -75,25 +73,30 @@ def render_template(template_name, fields):
 
     template = get_template(template_name)
 
-    all_fields = {
-        "shows_root": openpipe.show.get_show_root(),
-        "show_name": openpipe.show.get_current_show_name(),
-    }
-    all_fields.update(**fields)
-
     # Resolve nested partials
-    while "$" in template:
+    while "*" in template:
         template = flatten_template(template)
 
     tokens, partials = analyze_template(template)
 
     for token in tokens:
-        if token not in all_fields.keys():
+        if token not in fields.keys():
             raise KeyError("Missing required field named '%s'" % token)
 
-    #print(template)
-    #print(partials)
+    # First pass: render the fields
+    rendered = template.format(**fields)
 
-    rendered = template.format(**all_fields)
+    # Second pass: fill in the env variables
+    rendered = os.path.expandvars(rendered)
 
     return rendered
+
+
+def render_template_as_show_path(template_name, fields):
+
+    rendered = render_template(template_name, fields)
+
+    show_root = openpipe.show.get_show_root()
+    full_path = os.path.join(show_root, rendered)
+
+    return full_path
