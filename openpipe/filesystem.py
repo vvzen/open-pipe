c677e6a31afedca4aefc1e86fdb68146a560be69
Valerio Viperino <valerio.viperino@framestore.com>
2022-06-24 12:50:25 +0100

    Add support for printing correct num files/dirs

--- openpipe/filesystem.py
+++ openpipe/filesystem.py
@@ -15,8 +15,7 @@ class PrintTreeOptions(object):
         self.show_hidden_dirs = False
 
 
-def print_tree(input_dir, options=None, max_level=3,
-               _current_level=1, _num_files=0, _num_dirs=0):
+def print_tree(input_dir, options=None, max_level=3, _current_level=1):
     """A cheap and dirty way to print a directory tree, similarly to how the
     unix tool 'tree' does it, but dumbed down (for now).
 
@@ -29,14 +28,6 @@ def print_tree(input_dir, options=None, max_level=3,
     :param _current_level: ignore it. It's used by the implementation only and
         should not be used by the caller.
     :type current_level: int, optional
-
-    :param _num_files: ignore it. It's used by the implementation only and
-        should not be used by the caller.
-    :type _num_files: int, optional
-
-    :param _num_dirs: ignore it. It's used by the implementation only and
-        should not be used by the caller.
-    :type _num_dirs: int, optional
     """
 
     if not options:
@@ -52,6 +43,9 @@ def print_tree(input_dir, options=None, max_level=3,
         f for f in entries
         if os.path.isfile(os.path.join(input_dir, f))
     ])
+
+    num_files = 0
+    num_dirs = 0
 
     if options.show_files:
         for index, file in enumerate(files):
@@ -72,7 +66,7 @@ def print_tree(input_dir, options=None, max_level=3,
             separator = "  " * _current_level
             print(f"{separator} {prefix} {file}")
 
-            _num_files += 1
+            num_files += 1
 
             if not options.show_hidden_files and file.startswith("."):
                 continue
@@ -88,18 +82,22 @@ def print_tree(input_dir, options=None, max_level=3,
             if not options.show_hidden_dirs and directory.startswith("."):
                 continue
 
-            _num_dirs += 1
+            num_dirs += 1
 
             if _current_level == max_level:
                 continue
 
-            print_tree(os.path.join(input_dir, directory),
-                       options=options,
-                       max_level=max_level,
-                       _current_level=_current_level+1,
-                       _num_files=_num_files,
-                       _num_dirs=_num_dirs)
+            _num_files, _num_dirs = \
+                print_tree(os.path.join(input_dir, directory),
+                           options=options,
+                           max_level=max_level,
+                           _current_level=_current_level+1)
+
+            num_files += _num_files
+            num_dirs += _num_dirs
 
     # Exit condition
     if _current_level == 1:
-        print(f"\n{_num_dirs} directories, {_num_files} files")
+        print(f"\n{num_dirs} directories, {num_files} files")
+
+    return num_files, num_dirs
